e9f82d09f7aa4b858e7c43e79a4ef6ab60a7faad
Shaunus McNielsen <shaunson26@gmail.com>
2023-11-30 16:16:26 +0100

    Relative paths

--- menu-items.js
+++ menu-items.js
@@ -3,11 +3,11 @@ var menu = [
         pizza: "Margherita",
         ingredients: "Fresh tomatoes, fresh mozzarella, fresh basil",
         price: "$12.50",
-        image: '/assets/images/pizza.webp'
+        image: './assets/images/pizza.webp'
     }, {
         pizza: "Formaggio",
         ingredients: "Four cheeses (mozzarella, parmesan, pecorino, jarlsberg)",
         price: "$15.50",
-        image: '/assets/images/pizza.webp'
+        image: './assets/images/pizza.webp'
     }
 ]
