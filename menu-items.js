0928a5e4485b2a055e9cc7e6d63693c3d718d37c
Shaun Nielsen <shaunson26@gmail.com>
2023-11-30 16:09:11 +0100

    first commit

---
+++ menu-items.js
@@ -0,0 +1,13 @@
+var menu = [
+    {
+        pizza: "Margherita",
+        ingredients: "Fresh tomatoes, fresh mozzarella, fresh basil",
+        price: "$12.50",
+        image: '/assets/images/pizza.webp'
+    }, {
+        pizza: "Formaggio",
+        ingredients: "Four cheeses (mozzarella, parmesan, pecorino, jarlsberg)",
+        price: "$15.50",
+        image: '/assets/images/pizza.webp'
+    }
+]
